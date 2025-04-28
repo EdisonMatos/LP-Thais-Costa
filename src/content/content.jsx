94ad2da389944e90279a8c730676f4cd69722659
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-04-28 09:39:10 -0300

    fix: Converte em Webp

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -1,23 +1,23 @@
-import logo from "../assets/imgs/logo/logo.png";
-import heroImgPhone from "../assets/imgs/hero/personImg.png";
-import heroImgDesktop from "../assets/imgs/hero/bgHeroInfluencer.jpg";
-import bgHeroImg from "../assets/imgs/hero/bgHeroInfluencer.jpg";
+import logo from "../assets/imgs/logo/logo.webp";
+import heroImgPhone from "../assets/imgs/hero/personImg.webp";
+import heroImgDesktop from "../assets/imgs/hero/bgHeroInfluencer.webp";
+import bgHeroImg from "../assets/imgs/hero/bgHeroInfluencer.webp";
 import heroDefaultImg from "../assets/imgs/hero/lawHero.jpg";
-import backgroundPersonMobile from "../assets/imgs/hero/bgHeroMobile.jpg";
+import backgroundPersonMobile from "../assets/imgs/hero/bgHeroMobile.webp";
 
-import imgFeatures from "../assets/imgs/features/imgServices.jpg";
+import imgFeatures from "../assets/imgs/features/imgServices.webp";
 import imgDivisor from "../assets/imgs/features/imgDivisor.jpg";
 
-import aboutImg1 from "../assets/imgs/about/aboutImg.jpg";
+import aboutImg1 from "../assets/imgs/about/aboutImg.webp";
 import imgAbout1 from "../assets/imgs/about/aboutImg1.jpg";
 import imgAbout2 from "../assets/imgs/about/aboutImg2.jpg";
 import imgAbout3 from "../assets/imgs/about/aboutImg3.jpg";
 import imgAbout4 from "../assets/imgs/about/aboutImg4.jpg";
 import imgAbout5 from "../assets/imgs/about/aboutImg5.jpg";
-import aboutSocialImg from "../assets/imgs/about/aboutSocial.png";
+import aboutSocialImg from "../assets/imgs/about/aboutSocial.webp";
 import aboutInstagram from "../assets/imgs/about/imgAboutInstagram.jpg";
 
-import imgSteps from "../assets/imgs/steps/imgSteps.jpg";
+import imgSteps from "../assets/imgs/steps/imgSteps.webp";
 // import personImg from "../assets/imgs/hero/edvaldo.png";
 // import personImgPhone from "../assets/imgs/hero/edvaldo.png";
 
